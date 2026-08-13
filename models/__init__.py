5d603c2a43824722e89942d929ab77221d400402
Miki <mikiasworku87@gmail.com>
2026-08-13 11:28:56 +0300

    model

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,5 @@ from . import project_task_type
 from . import project_project
 from . import project_task
 from . import project_milestone
+from . import project_risk_issue
+from . import project_client_action
