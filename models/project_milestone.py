4926f1f6afb6884af21db7f5ea8d23a902ce3e3b
Miki <mikiasworku87@gmail.com>
2026-08-22 12:29:41 +0300

    milestone squence

--- models/project_milestone.py
+++ models/project_milestone.py
@@ -2,5 +2,6 @@ from odoo import models, fields
 
 class ProjectMilestone(models.Model):
     _inherit = 'project.milestone'
+    _order = 'x_phase_sequence asc, deadline asc, id asc'
 
     x_phase_sequence = fields.Integer('Phase Order', default=10, help='Order in which phases appear on the dashboard')
